2566d7accdc41459d7fe3d662ee705981542ad65
Meeka <rrgayhart@gmail.com>
2013-10-21 10:40:48 -0600

    Completes I0 Getting Started

---
+++ test/ideabox/idea_test.rb
@@ -0,0 +1,43 @@
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ideabox/idea'
+
+class IdeaTest < Minitest::Test
+  def test_basic_idea
+    idea = Idea.new("title", "description")
+    assert_equal "title", idea.title
+    assert_equal "description", idea.description
+  end
+
+  def test_ideas_can_be_liked
+    idea = Idea.new("diet", "carrots and cucumbers")
+    assert_equal 0, idea.rank # guard clause
+    idea.like!
+    assert_equal 1, idea.rank
+  end
+
+  def test_ideas_can_be_liked_more_than_once
+    idea = Idea.new("exercise", "stickfighting")
+    assert_equal 0, idea.rank # guard clause
+    5.times do
+      idea.like!
+    end
+    assert_equal 5, idea.rank
+  end
+
+  def test_ideas_can_be_sorted_by_rank
+    diet = Idea.new("diet", "cabbage soup")
+    exercise = Idea.new("exercise", "long distance running")
+    drink = Idea.new("drink", "carrot smoothy")
+
+    exercise.like!
+    exercise.like!
+    drink.like!
+
+    ideas = [diet, exercise, drink]
+
+    assert_equal [exercise, drink, diet], ideas.sort
+  end
+
+end
